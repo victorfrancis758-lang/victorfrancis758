6c07b59905f942cbc58f7e9b445a78ce831269dc
victorfrancis758-lang <victorfrancis758@gmail.com>
2026-03-26 10:58:19 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Update system packages
 apt-get update
-
-# Install Tesseract OCR
 apt-get install -y tesseract-ocr
-
-# Optional: English language support
 apt-get install -y tesseract-ocr-eng
